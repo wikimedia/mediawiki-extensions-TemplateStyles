1615767ae710825cbed31c31cb23f3471d1d6f0a
Bryan Davis <bd808@wikimedia.org>
2016-04-20 14:08:43 -0600

    Pretty up the test suite
    Add some comments and tweak the formatting of the test suite. Some
    things that were hardcoded can now be tweaked by the
    parse->render->verify tests if desired.
    
    Change-Id: I36abc9fa2b9971d6b92d5714c4583dea6ad26b88

--- tests/CSSParseRenderTest.php
+++ tests/CSSParseRenderTest.php
@@ -5,233 +5,241 @@
  */
 class CSSParseRenderTest extends MediaWikiTestCase {
 
-	protected function setUp() {
-		parent::setUp();
-	}
-
-	public static function provideCSSParser() {
-		return [
-			[	'test' => 'Bare declaration',
-				'expect' => '',
-				'css' => <<<FIN
-					prop: val;
-FIN
-			],
-			[	'test' => 'Blacklisted property',
-				'expect' => '.X .sel {good:123;} ',
-				'css' => <<<FIN
-					.sel {
-					  good: 123;
-					  -evil: "boo";
-				}
-FIN
-			],
-			[	'test' => 'Case insensivity',
-				'expect' => '@media screen { .X .sel1 {prop:WhiteListed(foo);} } ',
-				'css' => <<<FIN
-					@MEDIA screen {
-						.sel1 {
-							prop: WhiteListed(foo);
-							-EVIL: evil;
-						}
-					}
-FIN
-			],
-			[	'test' => 'Comment trickery',
-				'expect' => '.X .sel1 {} .X .sel2 .sel3 {prop3:val3;} ',
-				'css' => <<<FIN
-					.sel1 {
-						-ev/* x */il: evil;
-					}
-					.sel2 /* { prop2: val2; } */
-					.sel3 {
-						prop3: val3;
-					} /* unfinishe
-FIN
-			],
-			[	'test' => 'Complex selectors',
-				'expect' => '.X .sel1[foo=\'ba{r\'] #id a.foo::hover {prop1:val1;} ',
-				'css' => <<<FIN
-					.sel1[foo='ba{r'] #id a.foo::hover {
-						prop1: val1;
-					}
-FIN
-			],
-			[	'test' => 'Edge cases',
-				'expect' => '.X :sel {} ',
-				'css' => <<<FIN
-					:sel {
-					}
-FIN
-			],
-			[	'test' => 'Function in function',
-				'expect' => '.X .sel1 {} ',
-				'css' => <<<FIN
-					.sel1 {
-						prop1: whitelisted(1, evil(2));
-					}
-FIN
-			],
-			[	'test' => 'Incomplete rule',
-				'expect' => '.X .sel {prop:val;} ',
-				'css' => <<<FIN
-					.sel {
-					  prop: val;
-FIN
-			],
-			[	'test' => 'Media block',
-				'expect' => '.X .sel2 {prop2:val2;} @media print { .X .sel1 {prop1:val1;} } ',
-				'css' => <<<FIN
-					@media print {
-					  .sel1 {
-						prop1: val1;
-					  }
-					}
-
-					.sel2 {
-					  prop2: val2;
-					}
-FIN
-			],
-			[	'test' => 'Multiple rules',
-				'expect' => '.X .sel1 A {prop1:val1;} .X T.sel2 {prop2:val2;} ',
-				'css' => <<<FIN
-					.sel1 A {
-					  prop1: val1;
-					}
-
-					T.sel2 {
-					  prop2: val2;
-					}
-FIN
-			],
-			[	'test' => 'Multiple selectors',
-				'expect' => '.X .sel1,.X TD .sel2["a,comma"],.X #id {prop:val;} ',
-				'css' => <<<FIN
-					.sel1, TD .sel2["a,comma"], #id {
-					  prop: val;
-					}
-FIN
-			],
-			[	'test' => 'No selector',
-				'expect' => '{prop1:val1;} ',
-				'css' => <<<FIN
-					{
-						prop1: val1;
-					}
-FIN
-			],
-			[	'test' => 'Not a declaration',
-				'expect' => '.X .sel {prop:val;} ',
-				'css' => <<<FIN
-					.sel {
-					  not a declaration;
-					  prop: val;
-					}
-FIN
-			],
-			[	'test' => 'Obfuscated properties',
-				'expect' => '.X .sel {good:val2;} ',
-				'css' => <<<FIN
-					.sel {
-					  -\\065 vil: val1;
-					  go\\00006fd: val2;
-					}
-FIN
-			],
-			[	'test' => 'Rule within rule',
-				'expect' => '.X .sel1 {prop1:val1;} .X .sel3 {prop4:val4;} ',
-				'css' => <<<FIN
-					.sel1 {
-					  prop1: val1;
-					  .sel2 {
-						prop2: val2;
-					  }
-					  prop3: val3;
-					}
-
-					.sel3 {
-					  prop4: val4;
-					}
-FIN
-			],
-			[	'test' => 'String literals',
-				'expect' => '.X .sel {prop1:\'val1\';prop3:"v/**/al\"3";bad:"broken" ;} ',
-				'css' => <<<FIN
-					.sel {
-					  prop1: 'val1';
-					  prop3: "v/**/al\"3";
-					  bad: "broken
-					}
-FIN
-			],
-			[	'test' => 'Unsupported block',
-				'expect' => '.X .sel {prop2:val2;} ',
-				'css' => <<<FIN
-					@font-face {
-					  prop1: val1;
-					}
-
-					.sel {
-					  prop2: val2;
-					}
-FIN
-			],
-			[	'test' => 'Unwhitelisted function',
-				'expect' => '.X .sel {prop1:whitelisted(val1);} ',
-				'css' => <<<FIN
-					.sel {
-					  prop1: whitelisted(val1);
-					  prop2: evil(val2);
-					}
-FIN
-			],
-			[	'test' => 'Values',
-				'expect' => '.X .sel {prop:1em .5px 12% #FFF;} ',
-				'css' => <<<FIN
-					.sel {
-					  prop: 1em .5px 12% #FFF;
-					}
-FIN
-			],
-			[	'test' => 'Whitespace',
-				'expect' => '.X .sel1 #id{prop2:whitelisted ( val2 ) ;prop3:not whitelisted( val3 );} ',
-				'css' => <<<FIN
-					.sel1
-					#id{
-						-evil
-					:val1;
-					prop2/*
-						comment */: whitelisted ( val2 )
-					 ;prop3		:not/**/whitelisted( val3 );}
-FIN
-			]
-		];
-	}
-
 	/**
-	 * @dataProvider provideCSSParser
+	 * Parse a CSS string and then validate the rendered output.
+	 *
+	 * @param string $expect Expected CSS output from renderer
+	 * @param string $source Input for CSS parser
+	 * @param string $baseSelector Prefix for generated rules
+	 * @param array $functionWhitelist Allowed functions
+	 * @param array $propertyBlacklist Excluded properties
+	 * @dataProvider provideRendererAfterParse
 	 */
-	public function testCSSParser( $test, $expect, $source ) {
-
+	public function testRendererAfterParse(
+		$expect,
+		$source,
+		$baseSelector = '.X ',
+		$functionWhitelist = [ 'whitelisted' ],
+		$propertyBlacklist = [ '-evil' ]
+	) {
 		$tree = new CSSParser( $source );
-		$rules = $tree->rules( '.X ' );
+		$rules = $tree->rules( $baseSelector );
 		if ( !$rules ) {
-			$this->fail( "$test: Stylesheet did not parse." );
-			return;
+			$this->fail( "Failed to parse $source" );
 		}
 
 		$r = new CSSRenderer();
 		$r->add( $rules );
-		$css = $r->render( [ "whitelisted" ], [ "-evil" ] );
+		$css = $r->render( $functionWhitelist, $propertyBlacklist );
 
 		$this->assertEquals(
 			$expect,
-			preg_replace( '/[ \t\n]+/', ' ', $css ),
-			"$test: parse did not return expected output."
+			// Normalize whitespace inherited from the heredocs
+			preg_replace( '/[ \t\n]+/', ' ', $css )
 		);
-
 	}
 
+	/**
+	 * @see testRendererAfterParse
+	 */
+	public function provideRendererAfterParse() {
+		return [
+			'Bare declaration' => [
+				'expect' => '',
+				'css' => <<<CSS
+prop: val;
+CSS
+			],
+			'Blacklisted property' => [
+				'expect' => '.X .sel {good:123;} ',
+				'css' => <<<CSS
+.sel {
+	good: 123;
+	-evil: "boo";
+}
+CSS
+			],
+			'Case insensivity' => [
+				'expect' => '@media screen { .X .sel1 {prop:WhiteListed(foo);} } ',
+				'css' => <<<CSS
+@MEDIA screen {
+	.sel1 {
+		prop: WhiteListed(foo);
+		-EVIL: evil;
+	}
+}
+CSS
+			],
+			'Comment trickery' => [
+				'expect' => '.X .sel1 {} .X .sel2 .sel3 {prop3:val3;} ',
+				'css' => <<<CSS
+.sel1 {
+	-ev/* x */il: evil;
+}
+.sel2 /* { prop2: val2; } */
+.sel3 {
+	prop3: val3;
+} /* unfinishe
+CSS
+			],
+			'Complex selectors' => [
+				'expect' => '.X .sel1[foo=\'ba{r\'] #id a.foo::hover {prop1:val1;} ',
+				'css' => <<<CSS
+.sel1[foo='ba{r'] #id a.foo::hover {
+	prop1: val1;
+}
+CSS
+			],
+			'Edge cases' => [
+				'expect' => '.X :sel {} ',
+				'css' => <<<CSS
+:sel {
+}
+CSS
+			],
+			'Function in function' => [
+				'expect' => '.X .sel1 {} ',
+				'css' => <<<CSS
+.sel1 {
+	prop1: whitelisted(1, evil(2));
+}
+CSS
+			],
+			'Incomplete rule' => [
+				'expect' => '.X .sel {prop:val;} ',
+				'css' => <<<CSS
+.sel {
+	prop: val;
+CSS
+			],
+			'Media block' => [
+				'expect' => '.X .sel2 {prop2:val2;} @media print { .X .sel1 {prop1:val1;} } ',
+				'css' => <<<CSS
+@media print {
+	.sel1 {
+	prop1: val1;
+	}
+}
+
+.sel2 {
+	prop2: val2;
+}
+CSS
+		],
+			'Multiple rules' => [
+				'expect' => '.X .sel1 A {prop1:val1;} .X T.sel2 {prop2:val2;} ',
+				'css' => <<<CSS
+.sel1 A {
+	prop1: val1;
+}
+
+T.sel2 {
+	prop2: val2;
+}
+CSS
+			],
+			'Multiple selectors' => [
+				'expect' => '.X .sel1,.X TD .sel2["a,comma"],.X #id {prop:val;} ',
+				'css' => <<<CSS
+.sel1, TD .sel2["a,comma"], #id {
+	prop: val;
+}
+CSS
+			],
+			'No selector' => [
+				'expect' => '{prop1:val1;} ',
+				'css' => <<<CSS
+{
+	prop1: val1;
+}
+CSS
+			],
+			'Not a declaration' => [
+				'expect' => '.X .sel {prop:val;} ',
+				'css' => <<<CSS
+.sel {
+	not a declaration;
+	prop: val;
+}
+CSS
+			],
+			'Obfuscated properties' => [
+				'expect' => '.X .sel {good:val2;} ',
+				'css' => <<<CSS
+.sel {
+	-\\065 vil: val1;
+	go\\00006fd: val2;
+}
+CSS
+			],
+			'Rule within rule' => [
+				'expect' => '.X .sel1 {prop1:val1;} .X .sel3 {prop4:val4;} ',
+				'css' => <<<CSS
+.sel1 {
+	prop1: val1;
+	.sel2 {
+	prop2: val2;
+	}
+	prop3: val3;
+}
+
+.sel3 {
+	prop4: val4;
+}
+CSS
+			],
+			'String literals' => [
+				'expect' => '.X .sel {prop1:\'val1\';prop3:"v/**/al\"3";bad:"broken";} ',
+				'css' => <<<CSS
+.sel {
+	prop1: 'val1';
+	prop3: "v/**/al\"3";
+	bad: "broken
+}
+CSS
+			],
+			'Unsupported block' => [
+				'expect' => '.X .sel {prop2:val2;} ',
+				'css' => <<<CSS
+@font-face {
+	prop1: val1;
+}
+
+.sel {
+	prop2: val2;
+}
+CSS
+			],
+			'Unwhitelisted function' => [
+				'expect' => '.X .sel {prop1:whitelisted(val1);} ',
+				'css' => <<<CSS
+.sel {
+	prop1: whitelisted(val1);
+	prop2: evil(val2);
+}
+CSS
+			],
+			'Values' => [
+				'expect' => '.X .sel {prop:1em .5px 12% #FFF;} ',
+				'css' => <<<CSS
+.sel {
+	prop: 1em .5px 12% #FFF;
+}
+CSS
+			],
+			'Whitespace' => [
+				'expect' => '.X .sel1 #id{prop2:whitelisted ( val2 ) ;prop3:not whitelisted( val3 );} ',
+				'css' => <<<CSS
+.sel1
+#id{
+	-evil
+:val1;
+prop2/*
+	comment */: whitelisted ( val2 )
+	;prop3		:not/**/whitelisted( val3 );}
+CSS
+			],
+		];
+	}
 }
 
