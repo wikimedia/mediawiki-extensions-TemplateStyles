f9b195cb08545dc40de441e2316eed5f8beaa308
Umherirrender <umherirrender_de.wp@web.de>
2024-04-16 23:10:07 +0200

    build: Remove suppression of PhanUndeclaredProperty for Parser property
    Follow-Up: I701a27459704839f9f996a171b6982ed17cdd00b
    Change-Id: I7684384cd0b0ceab5668b35049e14454cd864e76

--- .phan/config.php
+++ .phan/config.php
@@ -2,14 +2,6 @@
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
-$cfg['suppress_issue_types'] = array_merge(
-	$cfg['suppress_issue_types'],
-	[
-		// \Parser->extTemplateStylesCache
-		'PhanUndeclaredProperty',
-	]
-);
-
 $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'],
 	[
